ad888708abc1adc6a2fd8ffead087bdab8cd70c6
Happy Marvin <HappyMarvin@yandex.ru>
2022-04-30 13:47:08 +0300

    Update src/Routes/PrivateOutlet.tsx
    Co-authored-by: Anton Subbotin <51344103+kotosha-real@users.noreply.github.com>

--- src/Routes/PrivateOutlet.tsx
+++ src/Routes/PrivateOutlet.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { PATHS } from "./paths";
 
-
 export function PrivateOutlet() {
     const auth = useAuth();
     return auth ? <Outlet /> : <Navigate to={PATHS.SIGN_IN} />;
