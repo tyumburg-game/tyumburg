a185781064dd59ba4f14317d826c7d93bf0a3d1d
Artem Beresnev <artembert@gmail.com>
2022-05-07 22:53:42 +0300

    feat(): apply useMemo hook in <ValidationMessage>

--- src/components/Input/ValidationMessage/ValidationMessage.tsx
+++ src/components/Input/ValidationMessage/ValidationMessage.tsx
@@ -1,9 +1,12 @@
 import block from "bem-cn";
-import { ReactNode } from "react";
+import { ReactNode, useMemo } from "react";
 import "./ValidationMessage.css";
 
 const b = block("validation-message");
 
 export default function ValidationMessage(props: { children: ReactNode }) {
-  return <div className={b()}>{props.children}</div>;
+  return useMemo(
+    () => <div className={b()}>{props.children}</div>,
+    [props.children]
+  );
 }
