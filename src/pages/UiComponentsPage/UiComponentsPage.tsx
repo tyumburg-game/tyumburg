bd9c51bae380f59e26634d789c7793dbe6c567ed
Vlasov Evgeniy <evgeniy.vlasov@rokolabs.com>
2022-05-10 00:27:10 +0500

    added dialog to demo

--- src/pages/UiComponentsPage/UiComponentsPage.tsx
+++ src/pages/UiComponentsPage/UiComponentsPage.tsx
@@ -8,48 +8,6 @@ import Modal from "components/Modal/Modal";
 const UiComponentsPage: FC = () => {
   return (
     <div className="grid">
-      <div className="buttons-grid">
-        <div>
-          <Button
-            mode="primary"
-            onClick={(event) => console.log("click", event)}
-          >
-            Primary
-          </Button>
-        </div>
-        <div>
-          <Button
-            mode="secondary"
-            className="parent-class-name"
-            onClick={(event) => console.log("click", event)}
-          >
-            Secondary
-          </Button>
-        </div>
-        <div>
-          <Button
-            mode="dangerous"
-            className="parent-class-name-one parent-class-name-two"
-            onClick={(event) => console.log("click", event)}
-          >
-            Dangerous
-          </Button>
-        </div>
-      </div>
-      <div className="inputs-grid">
-        <div>Default input</div>
-        <Input onChange={() => {}} value="input" label="First Name" />
-        <div>Invalid input</div>
-        <Input
-          onChange={() => {}}
-          value="input"
-          label="First Name"
-          mode="error"
-          comment={<ValidationMessage>invalid</ValidationMessage>}
-        />
-        <div>Type number</div>
-        <Input onChange={() => {}} value="3" label="Ages" type="number" />
-      </div>
       <div className="modals-grid">
         <Modal>
           <Modal.Header title="Modal Title"></Modal.Header>
@@ -59,8 +17,52 @@ const UiComponentsPage: FC = () => {
         <div>Dialog</div>
         <Modal type="dialog">
           <Modal.Header title="Modal Title"></Modal.Header>
-          <Modal.Content>Content</Modal.Content>
-          <Modal.Footer>Footer</Modal.Footer>
+          <Modal.Content>
+            <div className="inputs-grid">
+              <div>Default input</div>
+              <Input onChange={() => {}} value="input" label="First Name" />
+              <div>Invalid input</div>
+              <Input
+                onChange={() => {}}
+                value="input"
+                label="First Name"
+                mode="error"
+                comment={<ValidationMessage>invalid</ValidationMessage>}
+              />
+              <div>Type number</div>
+              <Input onChange={() => {}} value="3" label="Ages" type="number" />
+            </div>
+          </Modal.Content>
+          <Modal.Footer>
+            <div className="buttons-grid">
+              <div>
+                <Button
+                  mode="primary"
+                  onClick={(event) => console.log("click", event)}
+                >
+                  Primary
+                </Button>
+              </div>
+              <div>
+                <Button
+                  mode="secondary"
+                  className="parent-class-name"
+                  onClick={(event) => console.log("click", event)}
+                >
+                  Secondary
+                </Button>
+              </div>
+              <div>
+                <Button
+                  mode="dangerous"
+                  className="parent-class-name-one parent-class-name-two"
+                  onClick={(event) => console.log("click", event)}
+                >
+                  Dangerous
+                </Button>
+              </div>
+            </div>
+          </Modal.Footer>
         </Modal>
       </div>
     </div>
