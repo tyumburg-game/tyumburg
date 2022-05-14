e0d1a867aed49258793bd0485e0cbb7980e1c4c8
Artem Beresnev <artembert@gmail.com>
2022-05-14 23:18:16 +0300

    feat(): Create <ButtonsGroup> component

---
+++ src/components/ButtonsGroup/ButtonsGroup.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import block from "bem-cn";
+import "./ButtonsGroup.css";
+
+interface ButtonsGroupProps {
+  direction?: "horizontal" | "vertical";
+  className?: string;
+  children: ReactNode;
+}
+
+const b = block("buttons-group");
+
+export function ButtonsGroup(props: ButtonsGroupProps) {
+  const { direction = "horizontal", className = "", children } = props;
+  return <div className={b({ direction }).mix(className)}>{children}</div>;
+}
