39fed515c0bd9d1c029cd65d55a2ee4b0154a3ac
Artem Beresnev <artembert@gmail.com>
2022-05-15 00:19:34 +0300

    feat(): <Input> reserve place for comment

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -43,7 +43,7 @@ export default function Input(props: InputProps) {
           className={b("field", { mode })}
         />
       </div>
-      {comment && <div className={b("comment")}>{comment}</div>}
+      <div className={b("comment")}>{comment ?? null}</div>
     </div>
   );
 }
