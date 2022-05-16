662b9a7cd69fc95720e6f2a54fb414058c57fb5c
Vadim Tatarchuk <tatarchuk.v.b@gmail.com>
2022-05-16 08:40:55 +0500

    fix indent

--- src/pages/UiComponentsPage/UiComponentsPage.tsx
+++ src/pages/UiComponentsPage/UiComponentsPage.tsx
@@ -15,65 +15,65 @@ const UiComponentsPage: FC = () => {
 
   return (
     <ErrorBoundary>
-    <div className="grid">
-      <div className="modals-grid">
-        <Modal>
-          <Modal.Header title="Modal Title" />
-          <Modal.Content>Content</Modal.Content>
-          <Modal.Footer>Footer</Modal.Footer>
-        </Modal>
-        <div>Dialog</div>
-        <Modal fixed={false}>
-          <Modal.Header title="Modal Title" />
-          <Modal.Content>
-            <div className="inputs-grid">
-              <div>Default input</div>
-              <Input onChange={() => {}} value="input" label="First Name" />
-              <div>Invalid input</div>
-              <Input
-                onChange={handleFirstName}
-                value={text}
-                label="First Name"
-                mode="error"
-                comment={<ValidationMessage>invalid</ValidationMessage>}
-              />
-              <div>Type number</div>
-              <Input onChange={() => {}} value="3" label="Ages" type="number" />
-            </div>
-          </Modal.Content>
-          <Modal.Footer>
-            <div className="buttons-grid">
-              <div>
-                <Button
-                  mode="primary"
-                  onClick={(event) => console.log("click", event)}
-                >
-                  Primary
-                </Button>
+      <div className="grid">
+        <div className="modals-grid">
+          <Modal>
+            <Modal.Header title="Modal Title" />
+            <Modal.Content>Content</Modal.Content>
+            <Modal.Footer>Footer</Modal.Footer>
+          </Modal>
+          <div>Dialog</div>
+          <Modal fixed={false}>
+            <Modal.Header title="Modal Title" />
+            <Modal.Content>
+              <div className="inputs-grid">
+                <div>Default input</div>
+                <Input onChange={() => {}} value="input" label="First Name" />
+                <div>Invalid input</div>
+                <Input
+                  onChange={handleFirstName}
+                  value={text}
+                  label="First Name"
+                  mode="error"
+                  comment={<ValidationMessage>invalid</ValidationMessage>}
+                />
+                <div>Type number</div>
+                <Input onChange={() => {}} value="3" label="Ages" type="number" />
               </div>
-              <div>
-                <Button
-                  mode="secondary"
-                  className="parent-class-name"
-                  onClick={(event) => console.log("click", event)}
-                >
-                  Secondary
-                </Button>
+            </Modal.Content>
+            <Modal.Footer>
+              <div className="buttons-grid">
+                <div>
+                  <Button
+                    mode="primary"
+                    onClick={(event) => console.log("click", event)}
+                  >
+                    Primary
+                  </Button>
+                </div>
+                <div>
+                  <Button
+                    mode="secondary"
+                    className="parent-class-name"
+                    onClick={(event) => console.log("click", event)}
+                  >
+                    Secondary
+                  </Button>
+                </div>
+                <div>
+                  <Button
+                    mode="dangerous"
+                    className="parent-class-name-one parent-class-name-two"
+                    onClick={(event) => console.log("click", event)}
+                  >
+                    Dangerous
+                  </Button>
+                </div>
               </div>
-              <div>
-                <Button
-                  mode="dangerous"
-                  className="parent-class-name-one parent-class-name-two"
-                  onClick={(event) => console.log("click", event)}
-                >
-                  Dangerous
-                </Button>
-              </div>
-            </div>
-          </Modal.Footer>
-        </Modal>
+            </Modal.Footer>
+          </Modal>
+        </div>
       </div>
-    </div>
     </ErrorBoundary>
   );
 };
