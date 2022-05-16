509cde3c55de654e49a521088c3877e4a6d71e6a
Sorokin Vladimir <sorokin.v@docsvision.com>
2022-05-16 18:04:07 +0300

    remove memoize from useLink

--- src/hooks/use-link.ts
+++ src/hooks/use-link.ts
@@ -1,12 +1,11 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import { memoize } from "utils/memoize";
 
 export function useLink() {
   const navigate = useNavigate();
 
   return useCallback(
-    memoize((url: string) => () => navigate(url)),
+    (url: string) => () => navigate(url),
     []
   );
 }
