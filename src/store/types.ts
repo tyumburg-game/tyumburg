8d73bd5efe1d6ae415810b6cefeceecc4fabd1f2
Artem Beresnev <artembert@gmail.com>
2022-06-07 22:32:34 +0300

    remove unused code

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,10 +1,5 @@
 import { store } from "./index";
 
-export interface BaseAction<T> {
-  type: string;
-  payload?: T;
-}
-
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
