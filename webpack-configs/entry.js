fb7d14fa6f4f266a9ba45b7c02472fb21768de16
Artem Beresnev <artembert@gmail.com>
2022-07-10 12:24:43 +0300

    TYUM-60 exclude 'webpack-hot-middleware' from client build

--- webpack-configs/entry.js
+++ webpack-configs/entry.js
@@ -1,19 +1,21 @@
 // @ts-check
-const path = require("path");
 
 /**
  * @typedef {import('webpack').Configuration['entry']} Entry
  * @typedef {import('./types/index').ConfigPair<Entry>} EntryConfigPair
  */
 
+const production = process.env.NODE_ENV === "production";
+
 /**
  * @type {EntryConfigPair}
  */
 const entry = {
   client: [
-    "@gatsbyjs/webpack-hot-middleware/client?path=/__webpack_hmr",
+    !production &&
+      "@gatsbyjs/webpack-hot-middleware/client?path=/__webpack_hmr",
     "./src/client/index.tsx",
-  ],
+  ].filter((plugin) => Boolean(plugin)),
   server: ["./src/client/components/App/App.tsx"],
 };
 
